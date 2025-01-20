d444df26686fc96c936fb8ab7c7e1da59b80ecea
Robinson Lopez <robins.616009@gmail.com>
2025-01-20 14:24:55 -0500

    Creacion Interface Repositorio Cliente

--- src/main/java/gm/zona_fit/modelo/Cliente.java
+++ src/main/java/gm/zona_fit/modelo/Cliente.java
@@ -1,4 +1,22 @@
 package gm.zona_fit.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data //Con esta notacion se hacen todos los gets y sets automaticamente gracias a Lombok
+@NoArgsConstructor //Agregamos el constructor vacio a nuestra clase sin necesida de definirlos nosotros mismos
+@AllArgsConstructor //Agregamos contructor con tofos los argumentos para que podamos crera objetos con el nombre,id,etc.
+@ToString
+@EqualsAndHashCode
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Si es auntoincrementable es mejor usar IDENTITY
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
 }
---
+++ src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
@@ -0,0 +1,6 @@
+package gm.zona_fit.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
+}
