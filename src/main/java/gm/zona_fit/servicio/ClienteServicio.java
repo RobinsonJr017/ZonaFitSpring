fe1fa2a8676f3324d7b980e8b1f5648dce20c60f
Robinson Lopez <robins.616009@gmail.com>
2025-01-22 15:08:22 -0500

    Implementacion clase Servicio

---
+++ src/main/java/gm/zona_fit/servicio/ClienteServicio.java
@@ -0,0 +1,38 @@
+package gm.zona_fit.servicio;
+
+import gm.zona_fit.modelo.Cliente;
+import gm.zona_fit.repositorio.ClienteRepositorio;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServicio implements IClienteServicio{
+
+    @Autowired //anotacion que significa que se va autoinyectar una referencia (en este caso de la capa de repositorios)
+    private ClienteRepositorio clienteRepositorio;
+
+    @Override
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        return clientes;
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Integer idCliente) {
+        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null); //En el caso de que no encuentre nada gregue el valor de null
+        return cliente;
+    }
+
+    @Override
+    public void guardarCliente(Cliente cliente) {
+        clienteRepositorio.save(cliente);
+    }
+
+    @Override
+    public void eliminarCliente(Cliente cliente) {
+        clienteRepositorio.delete(cliente);
+    }
+}
--- src/main/java/gm/zona_fit/servicio/IClienteServicio.java
+++ src/main/java/gm/zona_fit/servicio/IClienteServicio.java
@@ -7,11 +7,11 @@ import java.util.List;
 public interface IClienteServicio {
     public List<Cliente> listarClientes();
 
-    public Cliente buscarClientePorId(Cliente cliente);
+    public Cliente buscarClientePorId(Integer idCliente);
 
     public void guardarCliente(Cliente cliente);
 
     public void eliminarCliente(Cliente cliente);
 
-    
+
 }
